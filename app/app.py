154e7d28c4853572f1381dce77310acf75f58127
Androidex147 <andres.mostany@gmail.com>
2025-12-01 10:44:19 +0100

    Refactor image loading and prediction logic

--- app/app.py
+++ app/app.py
@@ -1,4 +1,4 @@
- import os
+import os
 # Esto obliga a TensorFlow a usar el modo compatibilidad con versiones antiguas
 os.environ['TF_USE_LEGACY_KERAS'] = '1'
 
@@ -12,24 +12,23 @@ st.set_page_config(page_title="Reconocimiento Perros vs Gatos", page_icon="🐾"
 
 st.title("🐶 Detector de Mascotas 🐱")
 st.write("Usa la cámara para saber si es un perro o un gato.")
+
 # DEFINIMOS UNA FUNCIÓN PARA CARGAR EL MODELO Y GUARDARLO EN CACHE
-# Usamos cache para que no se cargue cada vez que detecta un movimiento
 @st.cache_resource
 def carga_modelo():
-    # Cargamos el modelo
     modelo = keras.models.load_model("st-app/keras_model.h5", compile=False)
-    # Carga las etiquetas de las clases
-    clases = open("app/labels.txt", "r").readlines()
+    with open("app/labels.txt", "r") as f:
+        clases = [line.strip() for line in f]  # Eliminar saltos de línea
     return modelo, clases
 
-
-# 1.CARGAMOS EL MODELO Y ETIQUETAS
+# 1. CARGAMOS EL MODELO Y LAS ETIQUETAS
 try:
     mi_modelo, nombre_clases = carga_modelo()
 except Exception as e:
     st.error(f"Error al cargar el modelo: {e}")
     st.stop()
-    # 2. CAPTURAMOS LA IMAGEN HACIENDO USO DE LA CÁMARA
+
+# 2. TOMAMOS IMAGEN DESDE LA CÁMARA
 imagen_camara = st.camera_input("Haz una foto")
 
 # 3. PREDICCIÓN
@@ -38,23 +37,23 @@ if imagen_camara is not None:
     imagen = ImageOps.fit(imagen, (224, 224), Image.Resampling.LANCZOS)
     imagen_array = np.asarray(imagen)
     normalizada_imagen_array = (imagen_array.astype(np.float32) / 127.5) - 1
-# Crear un array para un lote de 1 imagen. ndarray = N-Dimensional Array
-lote_imagenes = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
-lote_imagenes[0] = normalizada_imagen_array
 
-# Predicción
-resultados= mi_modelo.predict(lote_imagenes)
-indice = np.argmax(resultados[0])
-etiqueta = nombre_clases[indice]
-probabilidad = resultados[0][indice]
+    # Crear un batch con una sola imagen
+    lote_imagenes = np.expand_dims(normalizada_imagen_array, axis=0)
 
-st.divider() # Línea separadora visual
+    # Predicción
+    resultados = mi_modelo.predict(lote_imagenes)
+    indice = np.argmax(resultados[0])
+    etiqueta = nombre_clases[indice]
+    probabilidad = resultados[0][indice]
 
-if "Perro" in etiqueta:
+    st.divider()  # Línea separadora visual
+
+    if "Perro" in etiqueta:
         st.success(f"¡Es un **PERRO**! 🐶")
-        st.balloons() # Efecto visual
-else:
+        st.balloons()
+    else:
         st.success(f"¡Es un **GATO**! 🐱")
-        st.snow() # Efecto visual
+        st.snow()
 
-st.write(f"Estoy un {probabilidad:.2%} seguro.")
+    st.write(f"Estoy un {probabilidad:.2%} seguro.")
